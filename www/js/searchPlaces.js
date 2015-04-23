ffbcf61513a0e4f8e49040d1d7b371871d2de1fb
Andrei13 <leta1_13@uni.worc.ac.uk>
2015-04-23 21:08:22 +0100

    as

--- www/js/searchPlaces.js
+++ www/js/searchPlaces.js
@@ -58,7 +58,7 @@ function callback(results, status) {
      service.getDetails(request,function(place,status){
    if (status== google.maps.places.PlacesServiceStatus.OK) {
         var name=place.name;
-        $('#PlacesList').append('<li><h1>'+name+'</h1>');
+        $('#PlacesList').append('<li><h1>'+name+'</h1>').listview('refresh');;
         
       }
 });
