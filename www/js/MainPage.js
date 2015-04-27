b7bed23f2c389399db7ae3831d1db7d79d35162b
Andrei13 <leta1_13@uni.worc.ac.uk>
2015-04-27 14:42:37 +0100

    as

--- www/js/MainPage.js
+++ www/js/MainPage.js
@@ -53,7 +53,8 @@
            {
              if(state.types[key])
                {
-                 $('#'+key).attr("checked");
+                 $('#'+key).attr("checked").checkbox('refresh');
+
                }
             }
          //$('#types').controlgroup('refresh');
