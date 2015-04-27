3623a1d566421587771e26b73b54d1580043a6c3
Andrei13 <leta1_13@uni.worc.ac.uk>
2015-04-27 15:17:20 +0100

    as

--- www/js/MainPage.js
+++ www/js/MainPage.js
@@ -1,24 +1,41 @@
+//This is the code that controls the main window of the application
+//In here the user inputs all of his preferences for the search
 
-
+  //This function is called after the page has loaded
     function onLoad() {
+
+    //Restore the state of the main page
+    //This is mainly used when the user goes back to the main page from another page 
+    //so that the user inputs are preserved
       restoreState();
+
+      //Create a key in the local storage to check if the user uses this app for the frist time
         if(window.localStorage.getItem("firstuse")==null)
            {
             window.localStorage.setItem("firstuse","false");
            }
+
+       //Event listener for when the device is ready to be used
         document.addEventListener("deviceready", onDeviceReady, false);
+
+      //Click event for the Favourite button
         $('#favButton').click(function()
           {
+            //save the state of this page
             saveState();
+
+            //change the window to the Favourites window
             window.location="Favourites.html";
           });
         
+        //Click event for the History button
         $('#hisButton').click(function()
           {
             saveState();
             window.location="History.html";
           });
 
+         //Click event for the Search button
          $("#searchButton").click(function(e)
          {
             saveState();
@@ -27,8 +44,10 @@
 
 }
     
+    //Save the state of this page
     function saveState()
     {
+      //save the state as a JSON obj
       var state = {
           "distance": $( "#Distance" ).val(),
           "types":{
@@ -37,18 +56,26 @@
                    "night_club": $('#night_club').is(":checked"),
                  },
           "openNow": $('#openNowCheckbox').is(":checked")
-        }   
+        } 
+
+        //store the state as a key
         window.sessionStorage.setItem("state",JSON.stringify(state));
     }
 
+    //Restore the state of this page
     function restoreState()
     {
+      //Transform the state to a JSON obj
       var state= JSON.parse(window.sessionStorage.getItem("state"));
+
+
       if(state!=null)
       {
-
+         //set the distance
          $( "#Distance" ).val(String(state.distance));
          $("#Distance").selectmenu('refresh');
+
+         //set the types
          for(var key in state.types)
            {
              if(state.types[key])
@@ -58,7 +85,6 @@
 
                }
             }
-         //$('#types').controlgroup('refresh');
        }
 
     }
@@ -66,6 +92,8 @@
     // device APIs are available
     //
     function onDeviceReady() {   
+
+    //A message to appear only when the user uses the app for the first time
     if(window.localStorage.getItem("firstuse")=="false")
     {
         alert("welcome");
--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,12 @@
-setTimeout(function(){
-       window.location='MainPage.html';             
-    }, 2000);
+
+//Open the main page after 2 seconds
+
 
 function onLoad(){
 	cordova.exec(null, null, "SplashScreen", "show", []);
+	setTimeout(function(){
+       window.location='MainPage.html';             
+    }, 2000);
 	 }
 
 
