c5d250cab70c29940d93ed293e3dfa2940349a13
Andrei13 <leta1_13@uni.worc.ac.uk>
2015-05-06 23:52:19 +0100

    as

--- www/js/Favourites.js
+++ www/js/Favourites.js
@@ -6,12 +6,22 @@
   	while(window.localStorage.getItem("favourite"+i)!=null)
   	{
   		var item = JSON.parse(window.localStorage.getItem("favourite"+i));
-  		$('#placesList').append('<li id="favourite"'+i+'><h1>'+item.name+'</h1></li>').listview('refresh');
-  		for(var j=0;j<7;j++)
-         {
-           $('#favourite'+i).append('<li><h1 style="font-size: 50%">'+
-                     item.openinghours[j]+'</h1></li>').listview('refresh');
-          } 
+  		$('#placesList').append('<li id="favourite"'+i+'><h1>'+item.name+
+  			'</h1><h1 style="font-size: 50%">'+
+                     item.openinghours[0]+'</h1>
+                     <h1 style="font-size: 50%">'+
+                     item.openinghours[1]+'</h1>
+                     <h1 style="font-size: 50%">'+
+                     item.openinghours[2]+'</h1>
+                     <h1 style="font-size: 50%">'+
+                     item.openinghours[3]+'</h1>
+                     <h1 style="font-size: 50%">'+
+                     item.openinghours[4]+'</h1>
+                     <h1 style="font-size: 50%">'+
+                     item.openinghours[5]+'</h1>
+                     <h1 style="font-size: 50%">'+
+                     item.openinghours[6]+'</h1>
+                     </li>').listview('refresh');
           i++;
   }
 }
