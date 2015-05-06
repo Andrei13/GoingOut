92ed33b281f156ae5e6716d7039419c30a268197
Andrei13 <leta1_13@uni.worc.ac.uk>
2015-05-06 17:17:13 +0100

    as

--- www/js/ViewPlace.js
+++ www/js/ViewPlace.js
@@ -48,7 +48,7 @@ var directionsService = new google.maps.DirectionsService();
              directionsDisplay.setDirections(response);
              showOpeningHours(place)
              $('#info').append('<div id="addFav" style="font-size: 150%"><img src="img/star.png" style="width:40px;height:40px">Add the favourites</img></div>');
-             $('#addFav').click(addFavourite);
+             
                }
             });
 
@@ -75,6 +75,7 @@ function showOpeningHours(place)
         $('#openinghours').append('<li><h1 style="font-size: 150%">'+
                      place.opening_hours.weekday_text[i]+'</h1></li>').listview('refresh');
        }
+       $('#addFav').click(addFavourite);
   }
   catch(error)
   {
