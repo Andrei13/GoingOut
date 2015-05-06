321846d12f4291a3e5b6976d0eb377af464349e9
Andrei13 <leta1_13@uni.worc.ac.uk>
2015-05-06 23:56:39 +0100

    as

--- www/js/Favourites.js
+++ www/js/Favourites.js
@@ -7,21 +7,14 @@
   	{
   		var item = JSON.parse(window.localStorage.getItem("favourite"+i));
   		$('#placesList').append('<li id="favourite"'+i+'><h1>'+item.name+
-  			'</h1><h1 style="font-size: 50%">'+
-                     item.openinghours[0]+'</h1>
-                     <h1 style="font-size: 50%">'+
-                     item.openinghours[1]+'</h1>
-                     <h1 style="font-size: 50%">'+
-                     item.openinghours[2]+'</h1>
-                     <h1 style="font-size: 50%">'+
-                     item.openinghours[3]+'</h1>
-                     <h1 style="font-size: 50%">'+
-                     item.openinghours[4]+'</h1>
-                     <h1 style="font-size: 50%">'+
-                     item.openinghours[5]+'</h1>
-                     <h1 style="font-size: 50%">'+
-                     item.openinghours[6]+'</h1>
-                     </li>').listview('refresh');
+  			                              '</h1><h1 style="font-size: 50%">'+
+                     item.openinghours[0]+'</h1><h1 style="font-size: 50%">'+
+                     item.openinghours[1]+'</h1><h1 style="font-size: 50%">'+
+                     item.openinghours[2]+'</h1><h1 style="font-size: 50%">'+
+                     item.openinghours[3]+'</h1><h1 style="font-size: 50%">'+
+                     item.openinghours[4]+'</h1><h1 style="font-size: 50%">'+
+                     item.openinghours[5]+'</h1><h1 style="font-size: 50%">'+
+                     item.openinghours[6]+'</h1></li>').listview('refresh');
           i++;
   }
 }
