50b8a03b8483a9a3dabee6e05d8898a341d263c0
Andrei13 <leta1_13@uni.worc.ac.uk>
2015-05-06 09:51:33 +0100

    as

--- www/js/searchPlaces.js
+++ www/js/searchPlaces.js
@@ -71,7 +71,7 @@ function getPlaceDetails(thePlace,service)
       service.getDetails(request,function(place,status){
       
    if (status== google.maps.places.PlacesServiceStatus.OK) {
-    var _distance = distance(place.geometry.location.lat,place.geometry.location.lng,myPos.lat,myPos.lng);
+    var _distance = distance(place.geometry.location.C,place.geometry.location.j,myPos.C,myPos.j);
         Places[currentNrPlacesDisplayed] = {
         "place_id":place.place_id,
         "rating":place.rating,
